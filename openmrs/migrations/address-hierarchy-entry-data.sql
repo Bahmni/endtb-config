d2dc3da00dbb20522ca5b4cbc8866a0164dd1426
Preethi <preethi.ms34@gmail.com>
2015-11-20 15:15:26 +0530

    Removing liquibase migrations

--- openmrs/migrations/address-hierarchy-entry-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `address_hierarchy_entry` (`address_hierarchy_entry_id`,`name`,`level_id`,`parent_id`,`user_generated_id`,`latitude`,`longitude`,`elevation`,`uuid`) VALUES (6,'Peru',3,NULL,NULL,NULL,NULL,NULL,'379e8e56-dd36-42c5-9b35-04ec5842c123');
-INSERT INTO `address_hierarchy_entry` (`address_hierarchy_entry_id`,`name`,`level_id`,`parent_id`,`user_generated_id`,`latitude`,`longitude`,`elevation`,`uuid`) VALUES (7,'Huaraz',4,6,NULL,NULL,NULL,NULL,'94da5f1a-85f8-401e-b5f4-2b0120ff249a');
-INSERT INTO `address_hierarchy_entry` (`address_hierarchy_entry_id`,`name`,`level_id`,`parent_id`,`user_generated_id`,`latitude`,`longitude`,`elevation`,`uuid`) VALUES (8,'Huaraz',5,7,NULL,NULL,NULL,NULL,'0edd18af-d64d-4140-ba07-4345daf7b880');
-INSERT INTO `address_hierarchy_entry` (`address_hierarchy_entry_id`,`name`,`level_id`,`parent_id`,`user_generated_id`,`latitude`,`longitude`,`elevation`,`uuid`) VALUES (9,'Huaraz',6,8,NULL,NULL,NULL,NULL,'ded9cc95-d8ea-4a12-aebd-ebcfba6d8256');
-
