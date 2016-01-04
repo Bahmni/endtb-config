4f824f3106ad1061e07356d21523b49cdb9f5d04
Gourav Nema <mail@gourav.info>
2016-01-04 17:39:07 +0530

    +Banka | Mingle#416 | Drug-o-gram should not display "Stop" in the cell when the drug is stopped and re-started the same day or the next

--- openmrs/treatmentRegimenExtension/TreatmentRegimenExtension.groovy
+++ openmrs/treatmentRegimenExtension/TreatmentRegimenExtension.groovy
@@ -1,12 +1,16 @@
+import org.apache.log4j.Logger
 import org.joda.time.DateTime;
-import org.joda.time.Days;
+import org.joda.time.Days
+import org.joda.time.Hours;
 import org.openmrs.module.bahmniemrapi.drugogram.contract.RegimenRow;
 import org.openmrs.module.bahmniemrapi.drugogram.contract.TreatmentRegimen;
-import org.openmrs.module.bahmniemrapi.drugogram.contract.BaseTableExtension;
+import org.openmrs.module.bahmniemrapi.drugogram.contract.BaseTableExtension
+import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction
 
-import java.util.Date;
+public class TreatmentRegimenExtension extends BaseTableExtension<TreatmentRegimen> {
+	private static final org.apache.log4j.Logger log = Logger.getLogger(TreatmentRegimenExtension.class);
+	public static final int STOPING_INTERVAL_HOURS = 48;
 
-public class MonthCalculationExtension extends BaseTableExtension<TreatmentRegimen> {
 	@Override
 	public void update(TreatmentRegimen treatmentRegimen) {
 		Date treatmentStartDate = null;
@@ -16,6 +20,9 @@ public class MonthCalculationExtension extends BaseTableExtension<TreatmentRegim
 			e.printStackTrace();
 			return;
 		}
+
+		skipStopFlaging(STOPING_INTERVAL_HOURS, treatmentRegimen);
+
 		for (RegimenRow regimenRow : treatmentRegimen.getRows()) {
 			DateTime currentTreatmentDate = new DateTime(regimenRow.getDate());
 			Days days = Days.daysBetween(new DateTime(treatmentStartDate), currentTreatmentDate);
@@ -23,4 +30,26 @@ public class MonthCalculationExtension extends BaseTableExtension<TreatmentRegim
 			regimenRow.setMonth(month);
 		}
 	}
+
+	void skipStopFlaging(int restartTimeInterval, TreatmentRegimen treatmentRegimen) {
+		for (EncounterTransaction.Concept drug : treatmentRegimen.getHeaders()) {
+			RegimenRow lastStopRow = null;
+			for (RegimenRow row : treatmentRegimen.getRows()) {
+				Date rowDate = row.getDate();
+				if("Stop".equals(row.getDrugs().get(drug.getName()))) {
+					lastStopRow = row;
+				} else if(isStopFlagInInterval(row, drug, lastStopRow, restartTimeInterval)) {
+					lastStopRow.getDrugs().put(drug.getName(), null);
+				}
+			}
+		}
+	}
+
+	private boolean isStopFlagInInterval(RegimenRow row, EncounterTransaction.Concept drug, RegimenRow lastStopRow,
+										 int restartTimeInterval) {
+		return (row.getDrugs().get(drug.getName()) != null
+		&& lastStopRow != null
+		&& Hours.hoursBetween(new DateTime(lastStopRow.getDate()), new DateTime(row.getDate())).hours <= restartTimeInterval);
+	}
+
 }
