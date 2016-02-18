6744cf6dbd6cc73794c9837202b30ea2858e33f5
hemanths <hemanths@Administrators-MacBook-Pro.local>
2016-02-18 12:13:26 +0530

    Jaya, Hemanth | #1164 | updated current month of treatment calculation for program specific.

--- openmrs/observationsAdder/CurrentMonthOfTreatment.groovy
+++ openmrs/observationsAdder/CurrentMonthOfTreatment.groovy
@@ -2,37 +2,52 @@ import org.bahmni.module.bahmnicore.obs.ObservationsAdder;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.openmrs.Concept;
-import org.openmrs.Obs;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniObservation;
+import org.openmrs.module.emrapi.encounter.ConceptMapper;
+import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CurrentMonthOfTreatment implements ObservationsAdder {
 
-    public void addObservations(List<Obs> observations,  List<String> conceptNames) {
+    @Override
+    public void addObservations(Collection<BahmniObservation> observations, List<String> conceptNames) throws ParseException {
         if (conceptNames.contains("Current month of treatment")) {
-            Obs drugTreatmentStartDateObservation = getTreatmentStartDateObservation(observations);
+            BahmniObservation drugTreatmentStartDateObservation = getTreatmentStartDateObservation(observations);
 
             if (drugTreatmentStartDateObservation != null) {
-                String month = getCurrentMonthOfTreatment(drugTreatmentStartDateObservation.getValueDate());
+                String string = drugTreatmentStartDateObservation.getValueAsString();
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+                Date date = format.parse(string);
+                String month = getCurrentMonthOfTreatment(date);
 
                 Concept currentMonthOfTreatmentConcept = Context.getConceptService().getConcept("Current month of treatment");
+                EncounterTransaction.Concept concept = new ConceptMapper().map(currentMonthOfTreatmentConcept);
 
-                Obs currentMonthOfTreatmentObservation = createObservation(drugTreatmentStartDateObservation, currentMonthOfTreatmentConcept);
-                currentMonthOfTreatmentObservation.setValueText(month);
+                BahmniObservation currentMonthOfTreatmentObservation = createObservation(drugTreatmentStartDateObservation, concept);
+                currentMonthOfTreatmentObservation.setValue(month);
 
                 observations.add(currentMonthOfTreatmentObservation);
             }
         }
     }
 
-    private Obs createObservation(Obs parent, Concept questionConcept) {
-        Obs observation = new Obs(parent.getPerson(),
-                questionConcept, new Date(), parent.getLocation());
-        observation.setEncounter(parent.getEncounter());
-        observation.setCreator(parent.getCreator());
-        return observation;
+    private BahmniObservation createObservation(BahmniObservation obs, EncounterTransaction.Concept questionConcept) {
+        BahmniObservation bahmniObservation = new BahmniObservation();
+
+        bahmniObservation.setConcept(questionConcept);
+        bahmniObservation.setEncounterUuid(obs.getEncounterUuid());
+        bahmniObservation.setCreatorName(obs.getCreatorName());
+        bahmniObservation.setObservationDateTime(new Date());
+
+        return bahmniObservation;
     }
 
     private String getCurrentMonthOfTreatment(Date startDate) {
@@ -41,9 +56,9 @@ public class CurrentMonthOfTreatment implements ObservationsAdder {
         return String.format("%.1f", days.getDays() / 30.0F);
     }
 
-    private Obs getTreatmentStartDateObservation(List<Obs> observations) {
-        for (Obs obs : observations) {
-            if (obs.getConcept().getName().getName().equals("TUBERCULOSIS DRUG TREATMENT START DATE")) {
+    private BahmniObservation getTreatmentStartDateObservation(Collection<BahmniObservation> observations) {
+        for (BahmniObservation obs : observations) {
+            if (obs.getConcept().getName().equals("TUBERCULOSIS DRUG TREATMENT START DATE")) {
                 return obs;
             }
         }
