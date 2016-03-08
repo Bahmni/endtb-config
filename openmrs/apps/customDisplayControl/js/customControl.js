2d9325f60f8fe7fa8b80a209f0d3be4de2b96346
Mahesh Kumar <maheshkv@thoughtworks.com>
2016-03-08 17:40:11 +0530

    Mahesh | # 116 | Adding free text entries shown on Co-modbidities custom display control

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -45,15 +45,22 @@ angular.module('bahmni.common.displaycontrol.custom')
         var conceptNames = ["Diabetes Mellitus","Baseline, Chronic renal insufficiency","History of liver cirrhosis",
             "Baseline, Chronic obstructive pulmonary disease", "Baseline, Has cancer",
             "Baseline, Heart or atherosclerotic disease", "Baseline, Hepatitis B", "Baseline, Hepatitis C", "Baseline, Depression",
-            "Baseline, Has other psychiatric illness", "Baseline, Seizure disorder","Baseline, Pre-existing neuropathy"];
+            "Baseline, Has other psychiatric illness", "Baseline, Psychiatric illness type", "Baseline, Seizure disorder","Baseline, Pre-existing neuropathy", 
+	    "Baseline, Other pre-existing disease"];
         $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/coMorbidities.html";
         var allComorbidityConceptNames = [];
-        spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined, null, $scope.enrollment).then(function (response) {
+        spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, undefined, undefined, $scope.visitUuid, undefined, null, $scope.enrollment).then(function (response) {
             var observations = _.filter(response.data, function(obs){
-              return obs.value.name === "True";
+              return obs.value.name === "True" ;
             });
             _.each(observations, function(obs){
                 allComorbidityConceptNames.push(obs.conceptNameToDisplay);
+            });
+            var freeTextObservations = _.filter(response.data, function(obs){
+              return obs.concept.dataType === "Text";
+            });
+            _.each(freeTextObservations, function(obs){
+                allComorbidityConceptNames.push(obs.valueAsString);
             });
             $scope.allComorbidities = allComorbidityConceptNames.join(", ");
             $scope.hasNoValue = _.isEmpty(allComorbidityConceptNames);
