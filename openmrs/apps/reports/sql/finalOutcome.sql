89f85f11553d4c59b7ef745785fde5e55a936102
Mahesh Kumar <mahesh4gnu@gmail.com>
2016-05-25 22:00:54 +0530

    Mahesh | Adding custom sql for final outcome report

---
+++ openmrs/apps/reports/sql/finalOutcome.sql
@@ -0,0 +1,25 @@
+SELECT 	pi.identifier, 
+		drug.name AS drug_name, 
+		outcome.Outcome
+  FROM patient_program pp
+  JOIN program prog ON (pp.program_id = prog.program_id
+                        AND prog.name IN ('Basic management unit TB register',
+                                          'Second-line TB treatment register'))
+  JOIN episode_patient_program epp ON epp.patient_program_id = pp.patient_program_id
+  JOIN episode_encounter ee ON ee.episode_id = epp.episode_id
+  JOIN encounter e1 ON e1.encounter_id = ee.encounter_id
+  JOIN (select cast(COALESCE(orders.scheduled_date, orders.date_activated) AS date) AS start_date,orders.encounter_id,orders.order_id,orders.voided from orders where voided = 0) orders
+      ON (e1.encounter_id = orders.encounter_id
+                  AND orders.start_date>= '#startDate#'
+                  AND orders.start_date<= '#endDate#')
+  JOIN drug_order ON (orders.order_id = drug_order.order_id
+                      AND orders.voided IS FALSE)
+  JOIN drug ON (drug_order.drug_inventory_id=drug.drug_id)
+  JOIN patient_identifier pi ON pi.patient_id = e1.patient_id
+  LEFT JOIN (
+  SELECT cv2.concept_full_name as Outcome, o.encounter_id, ee.episode_id FROM obs o 
+	JOIN concept_view cv ON o.concept_id = cv.concept_id
+    JOIN concept_view cv2 ON o.value_coded = cv2.concept_id
+    JOIN episode_encounter ee ON ee.encounter_id = o.encounter_id
+	WHERE cv.concept_full_name = 'EOT, Outcome' AND o.voided = 0) outcome
+    ON outcome.episode_id = ee.episode_id;
