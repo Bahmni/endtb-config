a10cb832bc37be204f9c394209c9f16e3b73b3a0
Shireesha <bongaralashireesha@gmail.com>
2016-06-03 12:41:47 +0530

    Shireesha, Suman | #137 | Added Patient Information report in reports app

--- openmrs/apps/reports/sql/PersonReport.sql
+++ openmrs/apps/reports/sql/PersonReport.sql
@@ -4,21 +4,21 @@ SELECT
   o.given_name as 'given_name',
   DATE_FORMAT(o.birthdate, '%d/%b/%Y') as 'dob',
   o.age as 'age',
-  o.code as 'sex',
-  GROUP_CONCAT(DISTINCT(IF(pat.name = 'nationalIdentificationNumber', o.attr_value, NULL)) SEPARATOR ',') AS `id_nat`,
+  GROUP_CONCAT(DISTINCT (IF(o.gender = 'M',1, IF(o.gender = 'F',2, 3))) SEPARATOR ',') AS 'sex',
+  GROUP_CONCAT(DISTINCT(IF(pat.name = 'nationalIdentificationNumber', CONCAT('\"',o.attr_value,'\"'), NULL)) SEPARATOR ',') AS `id_nat`,
   DATE_FORMAT(o.date_created, '%d/%b/%Y') as `creation_date`,
-  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientAddress', o.attr_value, NULL)) SEPARATOR ',') AS `address_line1`,
-  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientAddressLine2', o.attr_value, NULL)) SEPARATOR ',') AS `address_line2`,
-  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientDistrict', o.attr_value, NULL)) SEPARATOR ',') AS `res_district`,
-  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientCountry', o.attr_value, NULL)) SEPARATOR ',') AS `res_country`,
+  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientAddress',CONCAT('\"',o.attr_value,'\"'), NULL)) SEPARATOR ',') AS `address_line1`,
+  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientAddressLine2',CONCAT('\"',o.attr_value,'\"'), NULL)) SEPARATOR ',') AS `address_line2`,
+  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientDistrict',CONCAT('\"',o.attr_value,'\"'), NULL)) SEPARATOR ',') AS `res_district`,
+  GROUP_CONCAT(DISTINCT(IF(pat.name = 'patientCountry',CONCAT('\"',o.attr_value,'\"'), NULL)) SEPARATOR ',') AS `res_country`,
   GROUP_CONCAT(DISTINCT(IF(pat.name = 'telephoneNumber', o.attr_value, NULL)) SEPARATOR ',') AS `telephone_number`
 FROM
   (SELECT
-     pi.identifier,
-     pn.given_name,
-     pn.family_name,
+     CONCAT('\"',pi.identifier, '\"') as identifier,
+     CONCAT('\"',pn.given_name,'\"') as given_name,
+     CONCAT('\"',pn.family_name, '\"') as family_name,
      floor(datediff(CURDATE(), p.birthdate) / 365) AS age,
-     mcd.code,
+     p.gender,
      p.birthdate,
      pa.date_created,
      attr.person_attribute_type_id,
@@ -29,7 +29,6 @@ FROM
      JOIN patient pa ON p.person_id = pa.patient_id and cast(pa.date_created AS DATE) BETWEEN '#startDate#' AND '#endDate#'
      JOIN person_name pn ON p.person_id = pn.person_id
      JOIN patient_identifier pi ON pa.patient_id = pi.patient_id
-     JOIN metadata_code_dictionary mcd ON p.gender = mcd.name
      LEFT OUTER JOIN person_address addr ON p.person_id = addr.person_id
      LEFT OUTER JOIN person_attribute attr ON p.person_id = attr.person_id and attr.voided = false
      LEFT OUTER JOIN person_attribute_type attr_type ON attr.person_attribute_type_id = attr_type.person_attribute_type_id
