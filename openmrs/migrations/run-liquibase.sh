32f7cffdb3e99876f3417e994a767e4ca051d714
bharatak <bharatak@thoughtworks.com>
2016-06-13 13:06:59 +0530

    Bharat| Updated the liquibase migration for creating add_concept_description stored proc

---
+++ openmrs/migrations/run-liquibase.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e -x
+
+. /etc/openmrs/bahmni-emr.conf
+if [ -f /etc/bahmni-installer/bahmni.conf ]; then
+. /etc/bahmni-installer/bahmni.conf
+fi
+
+java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$COMMON_CLASSPATH --changeLogFile=$1 --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update 
