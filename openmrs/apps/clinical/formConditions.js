0688f8aa16229c236d8e271f98c02fa395f68a98
SwathiVarkala <swathivarkala@gmail.com>
2016-06-13 16:08:17 +0530

    Swathi | #000 | Fixing the commit mistake

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -1,4 +1,4 @@
-hmni.ConceptSet.FormConditions.rules = {      //This is a constant that Bahmni expects
+Bahmni.ConceptSet.FormConditions.rules = {      //This is a constant that Bahmni expects
   'Baseline, Employment within the past year': function(formName, formFieldValues) {
         var conditions = {enable: [], disable: []};
         var conditionConcept = formFieldValues['Baseline, Employment within the past year'];
