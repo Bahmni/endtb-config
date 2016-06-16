916a28e2b5bd6ddb4f8b9dee52376a24c93dece4
jayasuganthi <jayasug@thoughtworks.com>
2016-06-16 22:29:32 +0530

    Jaya|#126| Fixing issues in AEI Report

--- openmrs/apps/reports/sql/SAE.sql
+++ openmrs/apps/reports/sql/SAE.sql
@@ -1,15 +1,12 @@
-SELECT pi.identifier,
+SELECT  pp.patient_program_id,
 drug.name AS drug_name,
-  prog.name AS program_name,
+ pi.identifier,
   pp.date_enrolled,
   orders.start_date,
   obs_en.`SAE Form, Date event became serious`,
   obs_en.`SAE Form, SAE term comprehensive AE list`
 
   FROM patient_program pp
-  JOIN program prog ON (pp.program_id = prog.program_id
-                        AND prog.name IN ('Basic management unit TB register',
-                                          'Second-line TB treatment register'))
   JOIN episode_patient_program epp ON epp.patient_program_id = pp.patient_program_id
   JOIN episode_encounter ee ON ee.episode_id = epp.episode_id
   JOIN encounter e1 ON e1.encounter_id = ee.encounter_id
@@ -17,7 +14,7 @@ drug.name AS drug_name,
       ON (e1.encounter_id = orders.encounter_id
                   AND orders.start_date>= '#startDate#'
                   AND orders.start_date<= '#endDate#'
-                  AND (cast(orders.startDate AS DATE) >= "2015-04-01"))
+                  AND (cast(orders.start_date AS DATE) >= "2015-04-01"))
   JOIN drug_order ON (orders.order_id = drug_order.order_id
                       AND orders.voided IS FALSE)
   JOIN drug ON (drug_order.drug_inventory_id=drug.drug_id)
@@ -25,7 +22,7 @@ drug.name AS drug_name,
   JOIN encounter e2 ON (e2.encounter_id=ee2.encounter_id)
   JOIN person ON person.person_id = e2.patient_id
   JOIN patient_identifier pi ON pi.patient_id = e2.patient_id
-  LEFT JOIN
+  JOIN
   (select root.encounter_id,root.obs_id as root_obs_id,o.obs_id,root.person_id,
       GROUP_CONCAT(DISTINCT(IF(cv.concept_full_name = 'SAE Form, Date event became serious',  o.value_datetime, NULL)) SEPARATOR ',') AS 'SAE Form, Date event became serious',
       GROUP_CONCAT(DISTINCT(IF(cv.concept_full_name = 'SAE Form, SAE term comprehensive AE list',  coalesce(answer.concept_short_name, answer.concept_full_name), NULL)) SEPARATOR ',') AS 'SAE Form, SAE term comprehensive AE list'
@@ -43,6 +40,5 @@ drug.name AS drug_name,
       and cv.concept_full_name in  ('SAE Form, Date event became serious', 'SAE Form, SAE term comprehensive AE list')
     group by o.obs_group_id ) obs_en ON (e2.encounter_id = obs_en.encounter_id and
                                         (`SAE Form, Date event became serious` BETWEEN orders.start_date AND DATE_ADD(orders.start_date,INTERVAL 210 DAY)
-                                         ) ) GROUP BY obs_en.root_obs_id,pi.identifier,
-         drug.drug_id;
+                                         ) ) GROUP BY obs_en.root_obs_id,pp.patient_program_id,drug.name;
 
