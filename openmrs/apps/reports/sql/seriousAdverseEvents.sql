7dc90bd1d2f62fe1a56478e5a047fa22395d2694
sourava <212sourav@gmail.com>
2016-06-29 17:31:07 +0530

    Sourav, Vikash | #1962 | When obs is voided, still its shown in serious adverse events report fixed

--- openmrs/apps/reports/sql/seriousAdverseEvents.sql
+++ openmrs/apps/reports/sql/seriousAdverseEvents.sql
@@ -3,16 +3,16 @@ SELECT
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, SAE ID number', first_child_obs.value_text, NULL)) SEPARATOR ',') AS 'SAE case Number',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, SAE term comprehensive AE list', (SELECT coalesce(concept_short_name, concept_full_name) from concept_view where concept_id = first_child_obs.value_coded), NULL)) SEPARATOR ',') AS 'Serious Adverse Event term',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Other SAE term', first_child_obs.value_text, NULL)) SEPARATOR ',') AS 'Other Serious Adverse event term not in comprehensive list',
-  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Event onset date', DATE_FORMAT(first_child_obs.value_datetime, '%d-%M-%Y'), NULL)) SEPARATOR ',') AS 'Date of SAE  onset',
-  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Date of SAE report', DATE_FORMAT(first_child_obs.value_datetime, '%d-%M-%Y'), NULL)) SEPARATOR ',') AS 'Date of SAE reporting',
+  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Event onset date', DATE_FORMAT(first_child_obs.value_datetime, '%d-%b-%Y'), NULL)) SEPARATOR ',') AS 'Date of SAE  onset',
+  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Date of SAE report', DATE_FORMAT(first_child_obs.value_datetime, '%d-%b-%Y'), NULL)) SEPARATOR ',') AS 'Date of SAE reporting',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Previously reported as AE', (SELECT coalesce(concept_short_name, concept_full_name) from concept_view where concept_id = first_child_obs.value_coded), NULL)) SEPARATOR ',') AS 'Was this event previously reported as an AE',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, AE ID if previously reported as AE', first_child_obs.value_text, NULL)) SEPARATOR ',') AS 'If Yes, what is the AE ID number',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Seriousness criteria', (SELECT coalesce(concept_short_name, concept_full_name) from concept_view where concept_id = first_child_obs.value_coded), NULL)) SEPARATOR ',') AS 'Seriousness criteria',
-  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Date event became serious', DATE_FORMAT(first_child_obs.value_datetime, '%d-%M-%Y'), NULL)) SEPARATOR ',') AS 'Date event became serious',
+  GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, Date event became serious', DATE_FORMAT(first_child_obs.value_datetime, '%d-%b-%Y'), NULL)) SEPARATOR ',') AS 'Date event became serious',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, TB drugs suspended due to this SAE', (SELECT coalesce(concept_short_name, concept_full_name) from concept_view where concept_id = first_child_obs.value_coded), NULL)) SEPARATOR ',') AS 'Were all anti-TB drug suspended due to this SAE?',
   GROUP_CONCAT(DISTINCT (IF(first_child_cn.name = 'SAE Form, SAE severity grade', (SELECT coalesce(concept_short_name, concept_full_name) from concept_view where concept_id = first_child_obs.value_coded), NULL)) SEPARATOR ',') AS 'SAE Grade (severity)'
 FROM obs top_level_obs
-  INNER JOIN patient p ON p.patient_id = top_level_obs.person_id AND p.voided = 0
+  INNER JOIN patient p ON p.patient_id = top_level_obs.person_id AND p.voided = 0 AND top_level_obs.voided = 0
   LEFT JOIN obs first_child_obs ON top_level_obs.obs_id = first_child_obs.obs_group_id AND first_child_obs.voided = 0
   LEFT JOIN concept_name top_level_cn ON top_level_cn.concept_id = top_level_obs.concept_id AND top_level_cn.concept_name_type = 'FULLY_SPECIFIED'
   LEFT JOIN concept_name first_child_cn ON first_child_cn.concept_id = first_child_obs.concept_id AND first_child_cn.concept_name_type = 'FULLY_SPECIFIED'
