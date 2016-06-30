da3cff1bca9bff13a9cce89f6f83110eeef4de67
Sudhamsh <ksudham@thoughtworks.com>
2016-06-30 12:13:52 +0530

    Sudhamsh | #1963 | Concepts missing codes for the exports

---
+++ openmrs/migrations/concept_ref_map_extended.sql
@@ -0,0 +1,144 @@
+call create_reference_mapping('Skin hyperpigmentation or hypopigmentation','84');
+
+call create_reference_mapping('Oral Discomfort','84');
+
+call create_reference_mapping('Anemia','103');
+
+call create_reference_mapping('Arthralgia','170');
+
+call create_reference_mapping('Peripheral neuropathy','185');
+
+call create_reference_mapping('Lactate','39');
+
+call create_reference_mapping('Prolonged QT Interval','14');
+
+call create_reference_mapping('Hearing impairment','40');
+
+call create_reference_mapping('Increased liver enzymes','64');
+
+call create_reference_mapping('Male','1');
+
+call create_reference_mapping('Female','2');
+
+call create_reference_mapping('P-Aminosalicylic Acid','44');
+
+call create_reference_mapping('patient','1');
+
+call create_reference_mapping('partner','2');
+
+call create_reference_mapping('Rifabutin','15');
+
+call create_reference_mapping('Rifapentine','16');
+
+call create_reference_mapping('Ciprofloxacin','32');
+
+call create_reference_mapping('Gatifloxacin','33');
+
+call create_reference_mapping('Ofloxacin','37');
+
+call create_reference_mapping('Clarythromicin','57');
+
+call create_reference_mapping('Thioacetazone','58');
+
+call create_reference_mapping('Sinus rhythm','1');
+
+call create_reference_mapping('Atrial fibrillation','2');
+
+call create_reference_mapping('Premature ventricular complexes','3');
+
+call create_reference_mapping('LAMIVUDINE','70');
+
+call create_reference_mapping('ABACAVIR','71');
+
+call create_reference_mapping('ZIDOVUDINE','72');
+
+call create_reference_mapping('DIDANOSINE','73');
+
+call create_reference_mapping('STAVUDINE','74');
+
+call create_reference_mapping('EMTRICITABINE','75');
+
+call create_reference_mapping('TENOFOVIR DISOPROXIL','76');
+
+call create_reference_mapping('EFAVIRENZ','80');
+
+call create_reference_mapping('NEVIRAPINE','81');
+
+call create_reference_mapping('ETRAVIRINE','82');
+
+call create_reference_mapping('ATAZANAVIR','90');
+
+call create_reference_mapping('DARUNAVIR','91');
+
+call create_reference_mapping('FOSAMPRENAVIR','92');
+
+call create_reference_mapping('LOPINAVIR / RITONAVIR','93');
+
+call create_reference_mapping('RITONAVIR','94');
+
+call create_reference_mapping('SAQUINAVIR','95');
+
+call create_reference_mapping('Raltegravir','60');
+
+call create_reference_mapping('Related Test, Hemoglobin','1');
+
+call create_reference_mapping('Related Test, Hematocrit','2');
+
+call create_reference_mapping('Related Test, Platelet count','3');
+
+call create_reference_mapping('Related Test, RBC count','4');
+
+call create_reference_mapping('Related Test, WBC count','5');
+
+call create_reference_mapping('Related Test, Absolute neutrophil count','6');
+
+call create_reference_mapping('Related Test, % neutrophils','7');
+
+call create_reference_mapping('Related Test, Potassium','8');
+
+call create_reference_mapping('Related Test, Magnesium','9');
+
+call create_reference_mapping('Related Test, Ionised Calcium','10');
+
+call create_reference_mapping('Related Test, Urea','11');
+
+call create_reference_mapping('Related Test, Creatinine','12');
+
+call create_reference_mapping('Related Test, Glucose','13');
+
+call create_reference_mapping('Related Test, Glucose','14');
+
+call create_reference_mapping('Related Test, HbA1c','15');
+
+call create_reference_mapping('Related Test, TSH','16');
+
+call create_reference_mapping('Related Test, Lipase','17');
+
+call create_reference_mapping('Related Test, AST','18');
+
+call create_reference_mapping('Related Test, ALT','19');
+
+call create_reference_mapping('Related Test, Total bilirubin','20');
+
+call create_reference_mapping('Related Test, Pregnancy test','21');
+
+call create_reference_mapping('Related Test, CD4 count','22');
+
+call create_reference_mapping('Related Test, Serum albumin','23');
+
+call create_reference_mapping('Related Test, Audiometry','24');
+
+call create_reference_mapping('Related Test, QtcF','25');
+
+call create_reference_mapping('Related Test, Other ECG','26');
+
+call create_reference_mapping('Related Test, Blood pressure','27');
+
+call create_reference_mapping('Related Test, Visual acuity','28');
+
+call create_reference_mapping('Related Test, Ishihara test','29');
+
+call create_reference_mapping('Related Test, BPNS','30');
+
+call create_reference_mapping('Related Test, EMG','31');
+
