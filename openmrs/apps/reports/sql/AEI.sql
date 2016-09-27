ad6a619a5351144b4a01579389aff6b3c71381f3
SwathiVarkala <swathivarkala@gmail.com>
2016-09-27 16:58:01 +0530

    Swathi | #000 | Changing the keywords to be capitals

--- openmrs/apps/reports/sql/AEI.sql
+++ openmrs/apps/reports/sql/AEI.sql
@@ -9,7 +9,7 @@ SELECT
   FROM patient_program pp
   JOIN episode_patient_program epp ON epp.patient_program_id = pp.patient_program_id
   JOIN episode_encounter ee ON ee.episode_id = epp.episode_id
-  JOIN (select cast(MIN(COALESCE(orders.scheduled_date, orders.date_activated)) AS date) AS start_date,ee.episode_id,orders.order_id from orders
+  JOIN (SELECT cast(MIN(COALESCE(orders.scheduled_date, orders.date_activated)) AS DATE ) AS start_date,ee.episode_id,orders.order_id FROM orders
         JOIN episode_encounter ee
         ON ee.encounter_id = orders.encounter_id AND orders.order_action != 'DISCONTINUE' AND orders.voided = 0
         GROUP BY ee.episode_id, orders.concept_id
@@ -17,27 +17,27 @@ SELECT
       ON (ee.episode_id = orders.episode_id
                   AND orders.start_date>= '#startDate#'
                   AND orders.start_date<= '#endDate#'
-                  AND (cast(orders.start_date AS DATE) >= '2015-04-01'))
+                  AND (CAST(orders.start_date AS DATE) >= '2015-04-01'))
   JOIN drug_order ON (orders.order_id = drug_order.order_id)
   JOIN drug ON (drug_order.drug_inventory_id=drug.drug_id)
   JOIN episode_encounter ee2 ON ee2.episode_id = epp.episode_id
   LEFT JOIN
-  (select root.encounter_id,root.obs_id as root_obs_id,o.obs_id,root.person_id,
+  (SELECT root.encounter_id,root.obs_id AS root_obs_id,o.obs_id,root.person_id,
       GROUP_CONCAT(DISTINCT(IF(cv.concept_full_name = 'AE Form, Date of AE onset',  o.value_datetime, NULL)) SEPARATOR ',') AS 'AE Form, Date of AE onset',
-      GROUP_CONCAT(DISTINCT(IF(cv.concept_full_name = 'AE Form, AE term comprehensive list',  coalesce(answer.concept_short_name, answer.concept_full_name), NULL)) SEPARATOR ',') AS 'AE Form, AE term comprehensive list'
-    from  obs root,
+      GROUP_CONCAT(DISTINCT(IF(cv.concept_full_name = 'AE Form, AE term comprehensive list',  COALESCE(answer.concept_short_name, answer.concept_full_name), NULL)) SEPARATOR ',') AS 'AE Form, AE term comprehensive list'
+    FROM  obs root,
            concept_view,
            concept_view cv,
            obs o LEFT JOIN
-           concept_view answer on (o.value_coded = answer.concept_id)
+           concept_view answer ON (o.value_coded = answer.concept_id)
     WHERE root.concept_id = concept_view.concept_id
-      and root.voided is false
-      and concept_view.concept_full_name = 'Adverse Events Template'
-      and root.obs_id =  o.obs_group_id
-      and cv.concept_id=o.concept_id
-      and o.voided is false
-      and cv.concept_full_name in  ('AE Form, Date of AE onset', 'AE Form, AE term comprehensive list')
-    group by o.obs_group_id ) obs_en ON (ee2.encounter_id = obs_en.encounter_id and
+      AND root.voided IS FALSE
+      AND concept_view.concept_full_name = 'Adverse Events Template'
+      AND root.obs_id =  o.obs_group_id
+      AND cv.concept_id=o.concept_id
+      AND o.voided IS FALSE
+      AND cv.concept_full_name IN  ('AE Form, Date of AE onset', 'AE Form, AE term comprehensive list')
+    GROUP BY o.obs_group_id ) obs_en ON (ee2.encounter_id = obs_en.encounter_id AND
                                         (`AE Form, Date of AE onset` BETWEEN orders.start_date AND DATE_ADD(orders.start_date,INTERVAL 210 DAY)
                                          AND `AE Form, AE term comprehensive list` IN('Prolonged QT interval','Hypokalemia','Hearing impairment ','Hypothyroidism','Increased liver enzymes','Optic nerve disorder','Anemia','Platelets decreased','Acute kidney injury','Prolonged (corrected) QT interval', 'Hypokalemia (K ≤ 3.4 mEq/L)', 'Hearing impairment (hearing loss)', 'Hypothyroidism', 'Increased liver enzymes (ALT increased or AST increased (>= 1.1 x ULN))', 'Optic nerve disorder (optic neuritis)', 'Anemia (Hb < 10.5 g/dL)','Platelets Decreased ( < 75000/mm^3 )', 'Acute kidney injury (acute renal failure)')) )
-GROUP BY pp.patient_program_id,drug.name,obs_en.root_obs_id;
+GROUP BY obs_en.root_obs_id, pp.patient_program_id, drug.name;
